d762c67a0e8ec1f3c4c6e6aaefe1a0be59a071e9
saad <saadc06@gmail.com>
2019-04-11 11:07:07 +0100

    First Commit

---
+++ src/main/java/microservices/book/gamification/repository/BadgeCardRepository.java
@@ -0,0 +1,22 @@
+package microservices.book.gamification.repository;
+
+import microservices.book.gamification.domain.BadgeCard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+
+/**
+ * Handles data operations with BadgeCards
+ */
+public interface BadgeCardRepository extends JpaRepository<BadgeCard, Long> {
+
+    /**
+     * Retrieves all BadgeCards for a given user.
+     * @param userId the id of the user to look for BadgeCards
+     * @return the list of BadgeCards, sorted by most recent.
+     */
+    List<BadgeCard> findByUserIdOrderByBadgeTimestampDesc(final Long userId);
+
+}
